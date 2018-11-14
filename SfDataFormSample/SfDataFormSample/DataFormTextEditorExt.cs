61472d2e065555bc4e339a50e7c8fa275ddb4e47
Jayaleshwari <jayaleshwarin@syncfusion.com>
2018-11-14 16:54:04 +0530

    Sample added

---
+++ SfDataFormSample/SfDataFormSample/DataFormTextEditorExt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using Syncfusion.iOS.DataForm;
+using Syncfusion.iOS.DataForm.Editors;
+using UIKit;
+
+namespace SfDataFormSample
+{
+    public class DataFormTextEditorExt : DataFormTextEditor
+    {
+        public DataFormTextEditorExt(SfDataForm dataForm) : base(dataForm)
+        {
+
+        }
+        protected override void OnInitializeView(DataFormItem dataFormItem, UITextField view)
+        {
+            base.OnInitializeView(dataFormItem, view);
+            if (dataFormItem.IsReadOnly)
+            {
+                // you can set alpha value for changing the visual appearance for the non-editable DataFormItem 
+                view.Alpha = (nfloat)0.5;                
+            }
+        }
+    }
+}
---
+++ SfDataFormSample/SfDataFormSample/ViewController.cs
@@ -0,0 +1,39 @@
+﻿using Foundation;
+using Syncfusion.iOS.DataForm;
+using System;
+using UIKit;
+
+namespace SfDataFormSample
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+            var dataForm = new SfDataForm(new CoreGraphics.CGRect(0, 45, this.View.Frame.Width, this.View.Frame.Height));
+            dataForm.RegisterEditor("Text", new DataFormTextEditorExt(dataForm));
+            dataForm.AutoGeneratingDataFormItem += OnAutoGeneratingDataFormItem;
+            dataForm.DataObject = new ContactInfo();
+            View.AddSubview(dataForm);
+        }
+
+        private void OnAutoGeneratingDataFormItem(object sender, AutoGeneratingDataFormItemEventArgs e)
+        {            
+            if (e.DataFormItem.Name == "Address")
+            {
+                e.DataFormItem.IsReadOnly = true;
+            }
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
